505ed8c18a54bff96c9a2e1e66f2d336713ebade
Roman <kutcu@mail.ru>
2016-12-15 13:13:05 +0300

    Create open-labyrinth.py

---
+++ open-labyrinth.py
@@ -0,0 +1,30 @@
+#Your code here
+#You can import some modules or create additional functions
+
+
+def checkio(maze_map):
+    visited=[[0 for col in range(12)] for row in range(12)]
+    def func(pos,maze_map,ans):
+        visited[pos[0]][pos[1]]=1
+        if pos == (10,10):
+            return ans
+        print ans,pos
+        if maze_map[pos[0] + 1][pos[1]] == 0 and visited[pos[0]+1][pos[1]]==0:
+            result=func((pos[0] + 1 , pos[1]),maze_map,ans + 'S')
+            if result:
+                return result
+        if maze_map[pos[0] - 1][pos[1]] == 0 and visited[pos[0] -1 ][pos[1]]==0:
+            result=func((pos[0] - 1 , pos[1]),maze_map,ans + 'N')
+            if result:
+                return result
+        if maze_map[pos[0] ][pos[1] + 1] == 0 and visited[pos[0]][pos[1]+1]==0:
+            result=func((pos[0]  , pos[1] + 1),maze_map,ans + 'E')
+            if result:
+                return result
+        if maze_map[pos[0] ][pos[1] - 1] == 0 and visited[pos[0]][pos[1]-1]==0:
+            result=func((pos[0]  , pos[1] - 1),maze_map,ans + 'W')
+            if result:
+                return result
+        return ''
+        
+    return func((1, 1),maze_map,'')
